75a6805365b40f39092f7734d459f7abba1b77df
Febro Herdyanto <febroherdyanto@outlook.com>
2023-06-18 09:43:52 +0700

    PM P12 - Memanggil Fragment antar Fragment - Completed

--- app/src/main/java/com/febroherdyanto/myflexiblefragment/CategoryFragment.kt
+++ app/src/main/java/com/febroherdyanto/myflexiblefragment/CategoryFragment.kt
@@ -8,32 +8,27 @@ import android.view.ViewGroup
 import android.widget.Button
 
 
-class HomeFragment : Fragment(), View.OnClickListener {
-
-
+class CategoryFragment : Fragment(),View.OnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_category, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btn_category:Button = view.findViewById(R.id.btn_category)
-        btn_category.setOnClickListener(this)
+        val btnDetailCategory:Button = view.findViewById(R.id.btn_detail_category)
+        btnDetailCategory.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        val categoryFragment = CategoryFragment()
-        val fragmentManager = parentFragmentManager
-        fragmentManager.beginTransaction().apply {
-            replace(R.id.frame_container,categoryFragment,CategoryFragment::class.java.simpleName)
-                .addToBackStack(null)
-                .commit()
+        if (v?.id == R.id.btn_detail_category){
+
         }
     }
+
 
 }
